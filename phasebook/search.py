db70db56b665e9610281652fa35755d57d1078d6
oryango <76882639+oryango@users.noreply.github.com>
2022-10-05 14:19:30 +0800

    Cleaned up code

--- phasebook/search.py
+++ phasebook/search.py
@@ -12,21 +12,6 @@ def search():
 
 
 def search_users(args):
-    """Search users database
-
-    Parameters:
-        args: a dictionary containing the following search parameters:
-            id: string
-            name: string
-            age: string
-            occupation: string
-
-    Returns:
-        a list of users that match the search parameters
-    """
-
-    # Implement search here!
-
     results = []
     priority = []
     arg = ['id','name','age','occupation']
@@ -39,41 +24,12 @@ def search_users(args):
                     priority.append(index)
                     break
 
-
     ordered_results = []
     for index in range(4):
         for iindex in range(len(results)):
             if(priority[iindex] == index):
                 ordered_results.append(results[iindex])
 
-    """
-    if('id' in args):
-        for user in USERS:
-            if(args['id'] == user['id'] and user not in results):
-                results.append(user)
-
-    if('name' in args):
-        param = args['name'].lower()
-        for user in USERS:
-            attribute = user['name'].lower()
-            if(param in attribute and user not in results):
-                results.append(user)
-
-    if('age' in args):
-        ages = int(args['age'])
-        ages = [ages-1, ages, ages+1]
-        for user in USERS:
-            if(int(user['age']) in ages and user not in results):
-                results.append(user)
-
-    if('occupation' in args):
-        param = args['occupation'].lower()
-        for user in USERS:
-            attribute = user['occupation'].lower()
-            if(param in attribute and user not in results):
-                results.append(user)
-   
-    """
     if(len(ordered_results) == 0):
         return USERS            
     return ordered_results
